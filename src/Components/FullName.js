e7e9ac9c6767bbaef0d460735f7738fbda68b4e8
AzizTN01 <azizbenrejen01@gmail.com>
2022-04-10 15:08:26 +0100

    commit

---
+++ src/Components/FullName.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Fullname = () => {
+ return (
+   <>
+    <p>Mohamed Aziz Ben Rejeb</p>
+   </>
+ );
+};
+export default Fullname;
---
+++ src/Components/ProfilePhoto.js
@@ -0,0 +1,10 @@
+import React from "react";
+import taswira from './picture/taswira.jpg';
+const ProfilePhoto = () => {
+ return (
+   <>
+     <img src={taswira} alt="taswira" style={{width: "150px" }} />
+   </>
+ );
+};
+export default ProfilePhoto;
